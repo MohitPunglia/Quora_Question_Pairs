3c14390eb12d7812c7d124508313d042dfae4af9
Mohit <mohit.punglia@gmail.com>
2025-03-13 21:37:55 +0530

    Added partial code

--- Partial_code.py
+++ Partial_code.py
@@ -24,3 +24,28 @@ new_df.head()
 
 # %%
 new_df.isnull().sum()
+
+# %%
+ques_df = new_df[["question1", "question2"]]
+ques_df.head()
+
+# %%
+from sklearn.feature_extraction.text import CountVectorizer
+
+questions = list(ques_df["question1"]) + list(ques_df["question2"])
+
+# %%
+cv = CountVectorizer(max_features=5000)
+q1_arr, q2_arr = np.vsplit(cv.fit_transform(questions).toarray(), 2)
+
+# %%
+questions[1:10]
+
+# %%
+temp_df1 = pd.DataFrame(q1_arr, index=ques_df.index)
+temp_df2 = pd.DataFrame(q2_arr, index=ques_df.index)
+temp_df = pd.concat([temp_df1, temp_df2], axis=1)
+temp_df.head()
+
+# %%
+temp_df.shape
