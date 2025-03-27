4e106166db16f2b05cb32183ac736b24af2e87f9
Mohit <mohit.punglia@gmail.com>
2025-03-27 20:13:58 +0530

    Added Partial code

--- partial_code.py
+++ partial_code.py
@@ -29,3 +29,24 @@ new_df.isnull().sum()  # checking for null values
 
 # %%
 new_df.duplicated().sum()  # checking for duplicate values
+
+# %%
+print(new_df["is_duplicate"].value_counts())
+print((new_df["is_duplicate"].value_counts() / new_df["is_duplicate"].count()) * 100)
+
+
+# %%
+qid = pd.Series(new_df["qid1"].tolist() + new_df["qid2"].tolist())
+print("Total number of unique questions", np.unique(qid).shape[0])
+
+x = qid.value_counts() > 1
+print("Number of questions getting repeated", x[x].shape[0])
+
+
+# %%
+# feature Engineering --> need to create new features
+new_df["q1_len"] = new_df["question1"].str.len()
+new_df["q2_len"] = new_df["question2"].str.len()
+
+# %%
+new_df.head()
